4e18e2c2ddce38af2bebfc8e5d57fb6d320ab9fe
Adnan Salem <adnan.salem@outlook.de>
2024-05-24 14:55:40 +0200

    dependencies 3

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,7 +7,6 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/ProductList" class="nav-link">Products</RouterLink>
-
       </nav>
     </div>
   </header>
