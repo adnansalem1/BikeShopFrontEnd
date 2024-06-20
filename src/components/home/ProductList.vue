098264143c21f632e80329a4eedb69db1b29ff74
Adnan Salem <adnan.salem@outlook.de>
2024-06-20 15:42:28 +0200

    Methode Update

--- src/components/home/ProductList.vue
+++ src/components/home/ProductList.vue
@@ -10,8 +10,31 @@
     </div>
     <ul>
       <li v-for="product in filteredProducts" :key="product.id" class="product-item">
-        {{ product.name }} - {{ product.beschreibung }} - {{ product.preis }} €
-        <button @click="deleteProduct(product.id)" class="delete-button">Delete</button>
+        <div v-if="editingProduct && editingProduct.id === product.id">
+          <input
+              v-model="editingProduct.name"
+              placeholder="Name"
+              class="input-field"
+          />
+          <input
+              v-model="editingProduct.beschreibung"
+              placeholder="Description"
+              class="input-field"
+          />
+          <input
+              v-model="editingProduct.preis"
+              type="number"
+              placeholder="Price"
+              class="input-field"
+          />
+          <button @click="saveProduct" class="save-button">Save</button>
+          <button @click="cancelEdit" class="cancel-button">Cancel</button>
+        </div>
+        <div v-else>
+          {{ product.name }} - {{ product.beschreibung }} - {{ product.preis }} €
+          <button @click="editProduct(product)" class="edit-button">Edit</button>
+          <button @click="deleteProduct(product.id)" class="delete-button">Delete</button>
+        </div>
       </li>
     </ul>
     <p v-if="filteredProducts.length === 0">Kein Produkt gefunden</p>
@@ -53,6 +76,7 @@ export default {
         beschreibung: "",
         preis: null,
       },
+      editingProduct: null,
       products: [],
       filteredProducts: [],
     };
@@ -61,7 +85,7 @@ export default {
     filterProducts() {
       console.log("Filtering products based on query:", this.searchQuery);
       const lowerCaseQuery = this.searchQuery.toLowerCase();
-      this.filteredProducts = this.products.map(p => ({ ...p })).filter(product => {
+      this.filteredProducts = this.products.filter(product => {
         console.log("Checking product:", product);
         return (
             product.name.toLowerCase().includes(lowerCaseQuery) ||
@@ -95,6 +119,30 @@ export default {
           console.error("Error adding product:", error);
         }
       }
+    },
+    editProduct(product) {
+      this.editingProduct = { ...product };
+    },
+    async saveProduct() {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_APP_BACKEND_BASE_URL}/anzeigen/${this.editingProduct.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this.editingProduct),
+        });
+        const updatedProduct = await response.json();
+        const index = this.products.findIndex(p => p.id === updatedProduct.id);
+        this.products.splice(index, 1, updatedProduct);
+        this.editingProduct = null;
+        this.filterProducts();
+      } catch (error) {
+        console.error("Error saving product:", error);
+      }
+    },
+    cancelEdit() {
+      this.editingProduct = null;
     },
     async deleteProduct(id) {
       try {
